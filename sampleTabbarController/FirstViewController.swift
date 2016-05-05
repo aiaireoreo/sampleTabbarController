2f377a90c53bab6cba2e1c684ecbde9905d87adb
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-06 05:42:38 +0800

    initial commit

--- sampleTabbarController/FirstViewController.swift
+++ sampleTabbarController/FirstViewController.swift
@@ -13,6 +13,26 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("FirstViewControllerが起動されました")
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+//        print("FirstViewControllerが表示されました")
+        
+    
+        //画面が表示されたとき
+        //AppDelegateにアクセスするための準備をして
+        var myAp = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        //プロパティの値を置き換える
+        myAp.myCount++
+        
+        //プロパティの値を読み出す
+        print("1画面目 count=\(myAp.myCount)")
+        
+
+
     }
 
     override func didReceiveMemoryWarning() {
--- sampleTabbarController/SecondViewController.swift
+++ sampleTabbarController/SecondViewController.swift
@@ -13,7 +13,31 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("SecondViewControllerが表示されました")
+
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        //        print("FirstViewControllerが表示されました")
+        tabBarItem.badgeValue = "✨"
+
+        
+        //画面が表示されたとき
+        //AppDelegateにアクセスするための準備をして
+        var myAp = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        //プロパティの値を置き換える
+        myAp.myCount++
+        
+        //プロパティの値を読み出す
+        print("2画面目 count=\(myAp.myCount)")
+
+        
+        
+        
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
